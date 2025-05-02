3fdaa0ed2f57fef5f528c0b184a503ef42bb929c
edupoly <info.edupoly@gmail.com>
2025-05-02 21:11:02 +0530

    counter-using-signals

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal,effect } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -7,8 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
-  count=0;
+  count=signal(0);
+  count2=signal(100);
+  constructor(){
+    effect(()=>{
+      console.log("count updated",this.count());
+    })
+    effect(()=>{
+      console.log("count2 updated",this.count2());
+    })
+  }
   inc(){
-    this.count++;
+    this.count.set(this.count()+1)
+    // this.count.update((ov)=>{return ov+10})
+  }
+  inc2(){
+    this.count2.set(this.count2()+10)
+    // this.count.update((ov)=>{return ov+10})
   }
 }
